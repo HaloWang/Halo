f1a10ac340ca9d89d78bdb8caedea9cd3d37a3d3
王策 <634692517@qq.com>
2016-01-12 16:09:50 +0800

    Add Alert

---
+++ Halo/Source/UIKit+Halo/Alert.swift
@@ -0,0 +1,53 @@
+
+import UIKit
+
+public class Alert {
+    
+    var title : String?
+    var message : String?
+    var style = UIAlertControllerStyle.Alert
+    var actions = [UIAlertAction]()
+    weak var alertController : UIAlertController?
+    
+    public class func showIn(vc:UIViewController) -> Alert {
+        
+        let alert = Alert()
+        
+        Last {
+            let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
+            alert.alertController = alertController
+            for action in alert.actions {
+                alertController.addAction(action)
+            }
+            vc.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
+        return alert
+    }
+    
+    public func title(title:String?) -> Self {
+        self.title = title
+        return self
+    }
+    
+    public func message(message:String?) -> Self {
+        self.message = message
+        return self
+    }
+    
+    public func style(style:UIAlertControllerStyle) -> Self {
+        self.style = style
+        return self
+    }
+    
+    public func addAction(title: String?, style: UIAlertActionStyle = UIAlertActionStyle.Default, handler: ((UIAlertAction) -> Void)?) -> Self {
+        let action = UIAlertAction(title: title, style: style) { [weak self] in
+            handler?($0)
+            self?.alertController?.dismissViewControllerAnimated(true, completion: nil)
+        }
+        actions.append(action)
+        return self
+    }
+}
+
+
