b0b05bce7af8e9412a59b3c2a623069e55ade1e7
王策 <634692517@qq.com>
2016-04-01 00:04:44 +0800

    改错字

--- Halo/Source/UIKit+Halo/UINavigationController+Halo.swift
+++ Halo/Source/UIKit+Halo/UINavigationController+Halo.swift
@@ -21,7 +21,7 @@ public extension UINavigationController {
         CGContextSetFillColorWithColor(ctx, color.CGColor)
         //	绘制方形
         CGContextFillPath(ctx)
-        //	将当前上线文以image形式输出
+        //	将当前上下文以image形式输出
         let image = UIGraphicsGetImageFromCurrentImageContext()
         //	停止绘制图片
         UIGraphicsEndImageContext()
